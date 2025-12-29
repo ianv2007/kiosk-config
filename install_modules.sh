3023c74d4986a706d6b2216d9467901925f166c6
Ian Vickers <wzrpt5f7d6@privaterelay.appleid.com>
2025-12-29 16:38:04 -0600

    Add files via upload

---
+++ install_modules.sh
@@ -0,0 +1,7 @@
+# Change to module directory
+cd ~/MagicMirror/modules
+# Download modules from Github
+git clone https://github.com/NicholasRasi/MMM-OnlineImagesViewer.git
+git clone https://github.com/Iketaki/MMM-WebView.git
+git clone https://github.com/slugmuffin/MMM-OnThisDayWikiApi
+
